25f13972ce8e6225ef84cd1d023a5d3e72c07768
BLTszendvics <balibalint0@gmail.com>
2023-09-10 20:34:59 +0200

    Puzzle update: no more bugs. Only things left: image spawn and responsivity

--- Game1/script.js
+++ Game1/script.js
@@ -1,7 +1,52 @@
-var images = document.getElementById("imgcontainer").children;
-var imageIDs = [11, 21];
+class Img {
+
+    Obj;
+    Id;
+    InPlace;
+    DefX;
+    DefY;
+
+    IPosX;
+    IPosY;
+
+    constructor(_Obj, _Id, _x, _y) {
+
+        this.Obj = _Obj;
+        this.Id = _Id;
+
+        this.InPlace = false;
+
+        this.DefX = _x;
+        this.DefY = _y;
+
+        this.ResetIPos();
+
+    }
+
+    ResetIPos() {
+
+        this.IPosX = -1;
+        this.IPosY = -1;
+
+    }
+
+}
+var timg = document.getElementById("imgcontainer").children;
+var Imgs = [new Img(timg[0], 11, 0, 0), new Img(timg[1], 21, 0, 0)];
+
+/*var imageIDs = [11, 21];
+
+let tid = 11;
+for (let i = 0; i < timg.length; i++) {
+
+    console.log(t1[i]);
+
+}*/
 
 var size = 2;
+
+var map = [];
+for (let y = 0; y < size; y++) { let f = [];  map.push(f); for (let x = 0; x < size; x++) {map[y].push(false)}}
 
 var good = 0;
 var maxGood = size * size;
@@ -18,7 +63,11 @@ var imageMainSize = imageSize * 2; // 2 * 2
 var mx;
 var my;
 
+var spawn = [100, 50, 2];
+
 function MouseMove(m) {
+
+    console.log(drag);
 
     if (drag) {
 
@@ -32,31 +81,47 @@ function MouseMove(m) {
 
 function GetMousePositions(m) {
 
-    mx = m.clientX;
-    my = m.clientY;
+    mx = m.clientX + window.scrollX;
+    my = m.clientY + window.scrollY;
 
 }
 
 function MouseDown(m, iIndex) {
-
+    console.log("down " + " " + drag + " " + this.iIndex + " " + iIndex);
     if (!drag) {
 
         drag = true;
         this.iIndex = iIndex;
+
+        if (Imgs[iIndex].InPlace) {
+
+            Imgs[iIndex].InPlace = false;
+            good--;
+
+        }
+        if (Imgs[iIndex].IPosX != -1) {
+
+            map[Imgs[iIndex].IPosX][Imgs[iIndex].IPosY] = false;
+            Imgs[iIndex].ResetIPos();
+
+        }
 
     }
 
 }
 
 function MouseUp(m, iIndex) {
+    //console.log(this.iIndex + " " + iIndex);
+    console.log("up");
+    if (this.iIndex != iIndex) {
+
+
+
+    }
 
     if (drag) {
 
-        GetMousePositions(m);
-
         drag = false;
-
-        console.log(mx + " " + my + " ");
 
         if (mx > imageMainX && mx < imageMainX + imageMainSize &&
             my > imageMainY && my < imageMainY + imageMainSize) {
@@ -65,11 +130,28 @@ function MouseUp(m, iIndex) {
             let iy = (my - imageMainY) - (my - imageMainY) % imageSize;
             
             let x = ix + imageMainX + imageSize/2;
-            let y = iy + imageMainY + imageSize/2;
+            let y = iy + imageMainY + imageSize / 2;
+            
+            let mapx = ix / imageSize;
+            let mapy = iy / imageSize; 
+
+            if (map[mapx][mapy]) {
+
+                Move(Imgs[this.iIndex].DefX+imageSize/2, Imgs[this.iIndex].DefX+imageSize/2, this.iIndex);
+
+            }
+            else {
+                
+                map[mapx][mapy] = true;
+
+                Imgs[this.iIndex].IPosX = mapx;
+                Imgs[this.iIndex].IPosY = mapy;
+
+                Move(x, y, this.iIndex);
     
-            Move(x, y, iIndex);
-    
-            Check(ix / imageSize, iy / imageSize);
+                Check(mapx, mapy);
+
+            }
             
         }
 
@@ -79,25 +161,23 @@ function MouseUp(m, iIndex) {
 
 function Move(x, y, i) {
 
-    images[i].style.left = (x - imageSize / 2) + "px";
-    images[i].style.top = (y - imageSize / 2) + "px";
+    Imgs[i].Obj.style.left = (x - imageSize / 2) + "px";
+    Imgs[i].Obj.style.top = (y - imageSize / 2) + "px";
 
 }
 
 function Check(x, y) {
-    
-    if (parseInt((x + 1) + "" + (y + 1)) == imageIDs[iIndex]) {
+
+    if (parseInt((x + 1) + "" + (y + 1)) == Imgs[iIndex].Id) {
         
         good++;
-        console.log(good);
+        Imgs[iIndex].InPlace = true;
 
     }
 
 }
 
-function Scroll() {
-
-    console.log(document.event);
+function Scroll(s) {
 
     MouseUp(window.event, this.iIndex);
 
