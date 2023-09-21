42e2e8733cd9eddaca3c7c402758357c760acdcc
BLTszendvics <80382954+BLTszendvics@users.noreply.github.com>
2023-09-21 14:13:07 +0200

    Wordle 1st version. There is literally nothing, I just created the files for it.

---
+++ Game2/script.js
@@ -0,0 +1,32 @@
+let container = document.getElementById("wordcontainer");
+
+let word = ["R", "A", "J", "M", "I"];
+
+let green = "greenyellow"
+let def = "burlywood";
+let red = "red";
+
+
+function keyPress(e) {
+
+    let enter = 13;
+    
+    if (e.keyCode == enter) {
+
+        Enter();
+
+    }
+
+}
+
+function Enter() {
+
+
+
+}
+
+function Check() {
+
+
+
+}
