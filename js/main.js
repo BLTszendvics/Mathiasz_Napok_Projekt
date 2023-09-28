8ab84be2e5369768c9527b1d6dba9e5ee2e597c9
vody05 <vodenicsar.rajmund_romeo@liveedu.mathiasz.hu>
2023-09-28 18:37:07 +0200

    Tractor & Combine Harvester menu has been updated, Fix for the section text, main.js has been added

---
+++ js/main.js
@@ -0,0 +1,43 @@
+function Tractor() {
+    document.getElementById("img1").src = '../img/john_deere_1026R.jpg'
+    document.getElementById("title1").innerText = "John Deere 1026R kompakt traktor"
+    document.getElementById("text1").innerText = "Az 1026R univerzális kompakt traktor tökéletes stabilitást és vezetési kényelmet nyújt. Ezen a feladatok széles skálájára tervezett kis kompakt traktoron a munkaeszközök és munkagépek könnyen és gyorsan cserélhetők. A standard felszereltség részét képezi a hátul és has alatt lévő kardántengely, a hátsó 3 pont felfüggesztés, a hidraulika csatlakozók, a könnyű működtetést elősegítő hidrosztatikus sebességváltó, a kezelő igényeihez állítható szervokormány és deluxe ülés, a négykerék hajtás, a differenciálzár és a deluxe lámpakészlet."
+
+    document.getElementById("img2").src = '../img/john_deere_3046R.jpg'
+    document.getElementById("title2").innerText = "John Deere 3046R kompakt traktor"
+    document.getElementById("text2").innerText = "A John Deere 3R sorozatú kompakt traktorokban a fejlett technológiájú funkciók széles választéka alapfelszereltségként került beépítésre. Ezek között megtalálható a négykerék hajtás, a digitális teljesítménykövető rendszer és a LoadMatch funkció is. Széleskörű felhasználhatóságuknak köszönhetően hosszútávon, költséghatékonyan alkalmazhatók települések, önkormányzatok sportpályaápolással, településfenntartással, városgondnoksággal, kommunális feladatok ellátásával foglalkozó vállalkozások, kertészetek és gazdálkodók számára egyaránt."
+
+    document.getElementById("img3").src = '../img/john_deere_3038E.jpg'
+    document.getElementById("title3").innerText = "John Deere 3038E traktor"
+    document.getElementById("text3").innerText = "Egy alapkivitelű kompakt traktorral valóban alacsonyabb teljesítményű és képességű gépet vásárol? Ha az egy John Deere E sorozatú traktor, akkor nem. A kedvező árú 3038E könnyű szerrel végrehajt számos feladatot. Ez a rátermett, praktikus és gazdaságos traktor Twin Touch pedálokkal, hidrosztatikus sebességváltóval és 3-pont függesztőszerkezettel rendelkezik. Takarékos üzemeltetési költsége révén hasznos eszköze a kertészeti és kisebb állattartó vállalkozásoknak."
+
+    document.getElementById("img4").src = '../img/john_deere_3045R.jpg'
+    document.getElementById("title4").innerText = "John Deere 3045R traktor"
+    document.getElementById("text4").innerText = "A 3R sorozatú traktor teljesítményének maximális kihasználásához a fejlett technológiájú funkciók teljes választékát építettük be az alapfelszereltség részeként. Ezek között megtalálható a négy-kerék hajtás, a digitális teljesítménykövető rendszer és a LoadMatch funkció: egy kiváló elektromos motorteljesítmény-szabályozó rendszer, amely minden alkalmazásban csúcsteljesítményt biztosít. A 3 hengeres dízelmotort nagy nyomatékrugalmasság jellemzi, amellyel terhelés alatt is lenyűgöző vontatási teljesítmény érhető el."
+
+    document.getElementById("img5").src = '../img/john_deere_4066M.jpg'
+    document.getElementById("title5").innerText = "John Deere 4066M traktor"
+    document.getElementById("text5").innerText = "A 4M sorozat egy teljes értékű mezőgazdasági traktor erejét és kezelhetőségét nyújtja. A 4 hengeres dízelmotorral, az eHydro sebességváltóval, a négy-kerék hajtással és az ergonomikus kezelőszerv konzollal minden az Ön rendelkezésére áll, ami a feladatok széles skálájának hatékony és könnyű végrehajtásához szükséges. Ez a modell választható manuális ás hidrostatikus váltóval is. Az erős, 4 hengers dízelmotor megfelelő nyomatékot biztosít meg a nehéz feladatokhoz is."
+}
+
+function Combine_Harvester() {
+    document.getElementById("img1").src = '../img/john_deere_W650.jpg'
+    document.getElementById("title1").innerText = "John Deere W650 kombájn"
+    document.getElementById("text1").innerText = "Kezdje a következő aratási szezont egy olyan kombájnnal, amely megkönnyíti Önnek, hogy a legtöbbet hozza ki a terményből, még nehéz körülmények között is. A W-sorozat nemcsak fokozza az Ön termelékenységét és hatékonyságát, hanem a termény és a szalma minősége is jobb. Számos új funkció támogatja az értékes idő megtakarítását és a hatékonyság növelését, ilyen például a ProDrive sebességváltó, ami automatikusan kapcsolja a sebességfokozatokat miközben Ön állandó sebességgel végezheti az aratást."
+
+    document.getElementById("img2").src = '../img/john_deere_W660.jpg'
+    document.getElementById("title2").innerText = "John Deere W660 kombájn"
+    document.getElementById("text2").innerText = "Üdvözöljük a szakma egyik legkényelmesebb munkakörnyezetében. A John Deere W660 sorozat tágas, Deluxe kivitelű vezetőfülkéjének teljes térfogata 3,30 m³. Számos átgondolt részlet segíti a gépkezelőt, hogy a legjobb formáját hozza, és még hosszú munkanapokon, a legnehezebb betakarítási körülmények között is friss lehessen. Köszönhetően a színezett üvegeknek, a vékony sarok-vázoszlopoknak, valamint a rendkívül ergonomikus kezelőszerveknek a kilátás mindig akadálytalan."
+
+    document.getElementById("img3").src = '../img/john_deere_T550.jpg'
+    document.getElementById("title3").innerText = "John Deere T550 kombájn"
+    document.getElementById("text3").innerText = "Az új T sorozat generációnak a megalkotásával mindent megtartottak abból, ami kiváló volt a T-sorozatban – de tökéletesítettek is rajta. A jól bevált terménytovábbító rendszer nem töri a szemeket és a szalmát, továbbá megnövelt teljesítményt kínál minden terményben és betakarítási körülmények között. A tökéletesített cséplőszerkezetet és tisztítórendszert ma már az egyik legnagyobb felületű a piacon."
+
+    document.getElementById("img4").src = '../img/john_deere_T560.jpg'
+    document.getElementById("title4").innerText = "John Deere T560 kombájn"
+    document.getElementById("text4").innerText = "Az új generációs John Deere T sorozat tervezése során maximálisan a gazdálkodók igényeire és kívánságaira koncentráltak a mérnökök: nagyobb teljesítmény, kisebb szemveszteség, egyszerű kezelhetőség, gyorsabb átszerelés másik terményre és a könnyebb karbantartás. A jól bevált terménytovábbító rendszer nem töri a szemeket és a szalmát, továbbá megnövelt teljesítményt kínál minden terményben és betakarítási körülmények között. A tökéletesített cséplőszerkezetet és tisztítórendszert ma már az egyik legnagyobb felület, ami a piacon kapható"
+
+    document.getElementById("img5").src = '../img/john_deere_T660.jpg'
+    document.getElementById("title5").innerText = "John Deere T660 kombájn"
+    document.getElementById("text5").innerText = "Az új John Deere T660 kombájn felszereltségét tekintve jobb, mint valaha: cséplőszerkezet, szalmarázó ládák, tisztítórendszer, magtartály és szalmaszecskázó. A termény levágásától a melléktermék kezelésig, a szántóföldtől az országútig, az első használattól a szezon végéig – az új T sorozat segítséget nyúlt intelligens megoldásaival, értékes jellemzőivel és innovatív szolgáltatásaival, így Ön gyorsabban, kényelmesebben és hatékonyabban tud dogozni."
+}
