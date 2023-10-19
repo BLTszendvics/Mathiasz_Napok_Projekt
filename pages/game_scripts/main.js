7d2f0fc1581343d68e80386e43aeba0acb53115a
vodenicsar_rajmund <vodenicsar.rajmund_romeo@liveedu.mathiasz.hu>
2023-10-19 11:32:08 +0200

    button design changes

--- pages/game_scripts/main.js
+++ pages/game_scripts/main.js
@@ -16,7 +16,7 @@ var tOut = "";
 function StartGames() {
 
     games = shuffle(games);
-    games = [2];
+    //games = [0];
 
     header.style.display = "none";
     footer.style.display = "none";
